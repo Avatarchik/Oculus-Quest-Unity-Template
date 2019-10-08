73a80ff67dda2f80709c03e0771d9cc81b4aa42f
Oscar Tong <moscartong@gmail.com>
2019-10-09 00:23:54 +0800

    feat: controller respond to button event
    https://developer.oculus.com/documentation/unity/latest/concepts/unity-ovrinput/

---
+++ Assets/OculusQuest/Scripts/QuestController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestController : MonoBehaviour
+{
+	public OVRInput.Controller Controller;
+
+
+	private Vector3 initScale;
+
+
+	private void Start() {
+		initScale = transform.localScale;
+	}
+
+
+	private void Update() {
+		if(OVRInput.Get(OVRInput.Button.One, Controller)) {
+			transform.localScale = initScale * 1.5f;
+		}
+		else {
+			transform.localScale = initScale;
+		}
+	}
+}

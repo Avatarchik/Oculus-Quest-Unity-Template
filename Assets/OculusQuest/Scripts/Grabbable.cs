98fb4fc139b2b1d4c3a2c3ca1b9641b8f234c826
Oscar Tong <moscartong@gmail.com>
2019-10-12 22:35:09 +0800

    fix; grabbable jitter issue
    https://forum.unity.com/threads/jittery-motion-on-oculus-quest.683704/#post-4576513

--- Assets/OculusQuest/Scripts/Grabbable.cs
+++ Assets/OculusQuest/Scripts/Grabbable.cs
@@ -5,6 +5,13 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 public class Grabbable : MonoBehaviour
 {
+	private void Start() {
+		// set rigidbody interpolation to solve jitter issue
+		// https://forum.unity.com/threads/jittery-motion-on-oculus-quest.683704/#post-4576513
+		Rigidbody rigidbody = GetComponent<Rigidbody>();
+		rigidbody.interpolation = RigidbodyInterpolation.Interpolate;
+	}
+
 	public void Ungrab(Vector3 velocity, Vector3 angularVelocity) {
 		Rigidbody rigidbody = GetComponent<Rigidbody>();
 		rigidbody.velocity = velocity;

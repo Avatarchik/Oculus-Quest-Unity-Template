9655240ed3e15cf55311fa19e0bf8798158a8341
Oscar Tong <moscartong@gmail.com>
2019-10-20 23:36:20 +0800

    wip: shot bullet out from the muzzle

---
+++ Assets/OculusQuest/Scripts/Sniper.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sniper : MonoBehaviour
+{
+	public GameObject BulletPrefab;
+
+	public Transform Muzzle;
+
+	public int RemainingBullet { get; private set; } = 0;
+}
